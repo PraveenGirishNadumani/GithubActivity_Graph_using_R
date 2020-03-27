95f5608a037f8512697491613fd14f924133222d
Praveen Girish Nadumani <praveen.gn@razorpay.com>
2020-03-27 22:52:50 +0530

    2nd commit with block dots on graphs

--- LastYearActivity.R
+++ LastYearActivity.R
@@ -3,4 +3,11 @@ library(jsonlite)
 
 # code to get the API payload/response from github API
 x <- fromJSON("https://api.github.com/repos/razorpay/razorpay-android-sample-app/stats/commit_activity")
-plot(unlist(x$days))
+x$days
+
+write.csv(x$days,"testfile.csv")
+p <- read.csv("testfile.csv")
+p
+
+plot(unlist(p),xlab = "days", ylab = "commits", col = "blue",pch = 19)
+
