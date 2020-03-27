d6f18e8507c83d6ecefe01cacc7fa398d26ef903
Praveen Girish Nadumani <praveen.gn@razorpay.com>
2020-03-27 23:33:28 +0530

    final commit, with weekly addition and deletion graph ploting.!

--- LastYearActivity.R
+++ LastYearActivity.R
@@ -1,13 +1,30 @@
 library(jsonlite)
 
+# code to get the API payload/response from github API last year activity data.
+year <- fromJSON("https://api.github.com/repos/razorpay/razorpay-android-sample-app/stats/commit_activity")
+year$days
 
-# code to get the API payload/response from github API
-x <- fromJSON("https://api.github.com/repos/razorpay/razorpay-android-sample-app/stats/commit_activity")
-x$days
+#writing the data to csv file using write.csv function 
+write.csv(year$days,"testfile.csv")
+#reading the data from .csv file
+yearData <- read.csv("testfile.csv")
 
-write.csv(x$days,"testfile.csv")
-p <- read.csv("testfile.csv")
-p
+#fetching the weekly data for number of addition and deletion from github API
+weeklyAddDel <- fromJSON("https://api.github.com/repos/razorpay/razorpay-android-sample-app/stats/code_frequency")
 
-plot(unlist(p),xlab = "days", ylab = "commits", col = "blue",pch = 19)
+#wrting weekly addition data to the csv file
+write.csv(weeklyAddDel[,2],"weeklyAddition.csv")
+
+#writing weekly deletion data to the csv file 
+write.csv(weeklyAddDel[,3],"weeklyDeletion.csv")
+
+weeklyAddData <- read.csv("weeklyAddition.csv")
+weeklyDelData <- read.csv("weeklyDeletion.csv")
+
+#ploting data for last year activity data 
+plot(unlist(yearData),xlab = "Days", ylab = "Commits per day", col = "blue",pch = 19, main = "Last Year Commit Data")
+
+#ploting data for weekly addition and deletion per week
+plot(weeklyAddData$X,weeklyAddData$x,xlab = "Number of weeks", ylab = "Number of addition", col = "blue", main = "Number Of Addition Per Week")
+plot(weeklyDelData,xlab = "Number of weeks", ylab = "Number of deletion", col = "blue", main = "Number Of Deletion Per Week")
 
